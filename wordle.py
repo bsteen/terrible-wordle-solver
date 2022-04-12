5755d2db6876118cc9746962583eff28f3ccbc0e
Benjamin Steenkamer <13274734+bsteen@users.noreply.github.com>
2022-04-12 03:10:29 -0400

    Slighly updated, still terrible
    - Now expects Wordle dictionary file (no longer uses dictionary library)
    - Unknown letters now use `?` instead of `*`
    - Third optional argument added for yellow letters (letters that must be in word, but position is unknown)
    - Added statistics printout

--- wordle.py
+++ wordle.py
@@ -1,13 +1,12 @@
 # (C) 2022 Benjamin Steenkamer
 
 import sys
-import nltk
-nltk.download("words")
-from nltk.corpus import words
+import time
 
-# Get user word (* = any letter)
-word = sys.argv[1]
-assert len(word) == 5
+start = time.time()
+# Get user word (? = any letter)
+user_word = sys.argv[1]
+assert len(user_word) == 5
 
 # Remove letters that are known not to be in word
 bad_letters = list(sys.argv[2])
@@ -15,23 +14,37 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 for bl in bad_letters:
     alphabet.remove(bl)
 
-def insert_letters(word):
+must_contain = []
+if len(sys.argv) == 4:
+    must_contain = list(sys.argv[3])
+
+# Load dictionary
+with open("wordle-dict.txt", "r") as f:
+    wordle_dict = f.read().splitlines()
+
+def insert_letters(combo):
     possible_words = []
     for alp in alphabet:
-        possible_words.append(word.replace("*", alp, 1))
+        possible_words.append(combo.replace("?", alp, 1))
     return possible_words
 
-guess_words = [word]
+letter_combos = [user_word]
 cont = True
 while cont:
     cont = False
-    for i in range(len(guess_words)):
-        if "*" in guess_words[i]:
-            guess_words += insert_letters(guess_words.pop(i))
+    for i in range(len(letter_combos)):
+        if "?" in letter_combos[i]:
+            letter_combos += insert_letters(letter_combos.pop(i))
             cont = True
+assert "?" not in letter_combos
 
-assert "*" not in guess_words
-print(len(guess_words)) #FIXME MANY possible words... too large to compute
-for w in guess_words:
-    if w in words.words():
-        print(w)
+# print(len(letter_combos)) #FIXME MANY possible words... too large to compute quickly
+possible_words = []
+for word in letter_combos:
+    if word in wordle_dict:
+        if all([mc in word for mc in must_contain]):
+            possible_words.append(word)
+possible_words.sort()
+
+end = time.time()
+print(f"{len(letter_combos)} letter combinations, {len(possible_words)} valid words, {end-start:0.3f} seconds\n{possible_words}")
